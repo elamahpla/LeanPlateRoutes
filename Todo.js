fb10643d716ea864404b503810e6563f4ed21f97
elamahpla <theamazingness@gmail.com>
2018-06-17 16:48:21 -0500

    Adding todo

---
+++ Todo.js
@@ -0,0 +1,4 @@
+// First: Setup a routing folder/file
+// Second: Fix npm scripts so I don't need multiple terminal tabs
+// Third: Fill out sass folder, including base settings & some base variables
+// Fourth: unknown, will try to update as needed
