82be3e0349d0d170ced0e79a9d5e929a31f3b814
Joey Robinson <theamazingness@gmail.com>
2019-06-05 14:28:06 -0400

    DEPRECATED

--- Todo.js
+++
@@ -1,7 +0,0 @@
-// First: Setup a routing folder/file
-// Second: Fix npm scripts so I don't need multiple terminal tabs --Done
-  // 2.1: Get script from othe projects to finish this.
-// Third: Fill out sass folder, including base settings & some base variables
-// Fourth: Add a 'primary' File for structure. This will be a layout with grid that can be shared across multiple routes
-// Fifth: I have too many 'dummy' files. Example: Hello.js & index.js. I need to cut both down, maybe?
-// -: unknown, will try to update as needed
